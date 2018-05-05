be5b9a80e8a55bb8996c191f732c675c7d9c361d
Liam K. Niehus-Staab <niehusst@cs.grinnell.edu>
2018-05-05 16:14:53 -0500

    finished serealization and stream constructor

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.io.*;
@@ -13,15 +15,17 @@ public class HuffmanTree {
 	 * @param map - a Map from data to its frequency
 	 */
 	public HuffmanTree(Map<Short, Integer> map) {
-		//turn map contents into LeafNodes and add to priority queue
+		//turn map contents into Nodes and add to priority queue
 		for(Map.Entry<Short, Integer> entry : map.entrySet()) {
-			Node leaf = new LeafNode(entry.getKey(), entry.getValue());
+			Node leaf = new Node(entry.getKey(), entry.getValue());
 			huffQueue.add(leaf);
 		}
-		
+		//add eof to queue
+		Short eof = 256;
+		huffQueue.add(new Node(eof, 1));
 		//turn queue into Huffman tree
 		while(huffQueue.size() >= 2) {
-			InteriorNode inner = new InteriorNode(huffQueue.poll(), huffQueue.poll());
+			Node inner = new Node(huffQueue.poll(), huffQueue.poll());
 			huffQueue.add(inner);
 		}
 	}
@@ -31,7 +35,73 @@ public class HuffmanTree {
 	 * @param in - stream from compressed file
 	 */
 	public HuffmanTree(BitInputStream in) {
+		huffQueue.add(HuffmanTreeH(in));	
+	}
+	
+	 /**
+	  * helper to the input stream constructor
+	  * @param in - stream from compressed file
+	  * @return - a Node that is to be inserted into the HuffmanTree
+	  */
+	private Node HuffmanTreeH(BitInputStream in) {
+		int ret;
 		
+		while ((ret = in.readBit()) != -1) {
+			if (ret == 0) {
+				//might not work correctly if just casting int to short
+				short data = (short) in.readBits(9);
+				//frequency is -1 because it doesn't matter when not building tree from Map
+				return new Node(data, -1);
+			} else {
+				Node left = HuffmanTreeH(in);
+				Node right = HuffmanTreeH(in);
+				Node newNode = new Node(left, right);
+				return newNode;
+			}
+		}
+		//if the stream starts as empty, return null
+		return null;
+	}
+	
+	private void serializeH(BitOutputStream out, Node cur) {
+		if (cur.isLeaf()) {
+			out.writeBit(0);
+			out.writeBits(cur.getData(), 9);			
+		} else {
+			out.writeBit(1);
+			serializeH(out, cur.getLeft());
+			serializeH(out, cur.getRight());			
+		}
+	}
+	
+	public void serialize(BitOutputStream out) {
+		Node cur = huffQueue.peek();
+		serializeH(out, cur);
+	}
+	
+	public void encode(BitInputStream in, BitOutputStream out) {
+		//first write the tree we are using to target
+		this.serialize(out);
+		//get the code for the data in Huffman tree and put in Hashmap
+		Node cur = huffQueue.peek();
+		Map<Short, ArrayList<Byte>> code = new HashMap<>();
+		ArrayList<Byte> path = new ArrayList<>();
+		encodeH(cur, path, code);
+	}
+	
+	//find all path codes and put into a map
+	private void encodeH(Node cur, ArrayList<Byte> path, Map<Short, ArrayList<Byte>> code) {
+		
+		if(cur.isLeaf()) {
+			//add path data pair to code map. Then clear path for next leaf??
+			code.put(cur.getData(), path);
+		} else {
+			//record the path taken in path list
+			path.add((byte) 0);
+			encodeH(cur.getLeft(), path, code);
+			path.add((byte) 1);
+			encodeH(cur.getLeft(), path, code);			
+		}
 	}
 
 }
--- src/Node.java
+++ src/Node.java
@@ -4,17 +4,90 @@
  * @author niehusst
  *
  */
-public interface Node {
+public class Node {
+	private Integer freq;
+	private Node left;
+	private Node right;
+	private Short data;
+	
+
+	/**
+	 * constructs an InteriorNode for HuffmanTree
+	 * @param left - the Node connected on left 
+	 * @param right - the Node connected on right 
+	 */
+	public Node(Node left, Node right) {
+		this.freq = left.getFreq() + right.getFreq();
+		this.right = right;
+		this.left = left;
+		this.data = null;
+	}
+	
+
+	/**
+	 * constructs a LeafNode 
+	 * @param data - the character to be stored at this node
+	 * @param freq - the frequency of the character in data
+	 */
+	public Node(Short data, Integer freq) {
+		this.data = data;
+		this.freq = freq;
+	}
+	
 	
 	/**
-	 * getter for the frequency of a Node
-	 * @return - Integer, the frequency
+	 * getter for freq field
+	 * @return - the Integer frequency as is some of the children of this Node
 	 */
-	public Integer getFreq();
+	public Integer getFreq() {
+		return this.freq;
+	}
 	
 	/**
-	 * tells you if a Node is a LeafNode
-	 * @return - true if is LeafNode, else false
+	 * getter for data field
+	 * @return - the Short data stored in this Node
 	 */
-	public Boolean isLeaf();
+	public Short getData() {
+		return this.data;
+	}
+	
+	
+	/**
+	 * tells you if Node is a LeafNode
+	 * @return true - this is a LeafNode
+	 */
+	public Boolean isLeaf() {
+		if (data == null)
+			return false;
+		else return true;
+	}
+	
+	/**
+	 * getter for right field
+	 * @return - the Node right
+	 */
+	public Node getRight() {
+		return this.right;
+	}
+	
+	/**
+	 * getter for left field
+	 * @return - the Node left
+	 */
+	public Node getLeft() {
+		return this.left;
+	}
+	
+	/**
+	 * returns the node from either left or right, as indicated by bin. (For serealization)
+	 * @param bin - a binary number (0 or 1). 0 indicates left, 1 indicates right
+	 * @return - a Node, either left or right
+	 */
+	public Node getDirection(byte bin) {
+		if(bin == 1) {
+			return this.getRight();
+		} else {
+			return this.getLeft();
+		}
+	}
 }
